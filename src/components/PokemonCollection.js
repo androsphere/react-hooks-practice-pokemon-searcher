4fc9dcd46d8250b9d0c1cda43256d951732d749f
Andrew MacIsaac <amacisaac95@gmail.com>
2023-09-30 20:11:10 -0600

    lab complete

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
 function PokemonCollection() {
+
+const API = "http://localhost:3001/pokemon";
+const [cardsToRender, setCardsToRender] = useState([])
+
+  useEffect(() => {
+    fetch(API)
+      .then((r) => r.json())
+      .then(setCardsToRender);
+  }, []);
+
+
+
+
+
   return (
     <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
+      {cardsToRender.map((card) =>(
+        <PokemonCard
+          key = {card.id}
+          name = {card.name}
+          hp = {card.hp}
+          image = {card.sprites.front}
+        />
+      ))
+
+      }
+    
     </Card.Group>
   );
 }
